369905ec162b5764cb427a529c7bc623eecb5f0e
Morgan Jackson <contactmojack@gmail.com>
2015-07-15 17:02:54 -0500

    mobile container fix needed;

--- js/app.js
+++ js/app.js
@@ -1,25 +1,13 @@
 $(document).ready(function() {
-	playSFX()
-	$('.hd').fadeIn(3000);
-	$('.video-container').YTPlayer({
-		videoId: 'qOnoVOCl1gg',
-		fitToBackground:true,// use as a background video
-		ratio: 16 / 9,
-		autoplay: 1,
-		start: 0,
-		repeat:true,
-		width: $(window).width(),
-		wrapperZIndex: 99,
-		playButtonClass: 'YTPlayer-play',
-		pauseButtonClass: 'YTPlayer-pause',
-		muteButtonClass: 'YTPlayer-mute',
-		volumeUpButtonClass: 'YTPlayer-volume-up',
-		volumeDownButtonClass: 'YTPlayer-volume-down'
-	});
+	$('body').css('display', 'none');
+	$('body').fadeIn(1500);
+	$('.link').click(function() {
+	event.preventDefault();
+	newLocation = this.href;
+	$('body').fadeOut(1500, newpage);
 });
 
-function playSFX () {
-	$('#sfx')[0].volume = 0.8;
-	$('#sfx')[0].load();
-	$('#sfx')[0].play();
-}
+function newpage() {
+	window.location = newLocation;
+	}
+});
